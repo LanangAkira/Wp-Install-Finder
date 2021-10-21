25dd2e73e129f75e8e88638879a28827c12c244e
LanangAkira <77384326+LanangAkira@users.noreply.github.com>
2021-10-21 22:41:28 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,7 @@
 import os
 print("\n\033[32;1mUpdating Wordlist...\n\n\033[0m")
-os.system('mv wordlist.txt wordlist_lama.txt')
+os.system('mv wordlist.txt wordlist_old.txt')
 os.system('wget https://raw.githubusercontent.com/LanangAkira/Wp-Install-Finder/main/wordlist.txt --no-check-certificate')
+os.system('mv wordlah.txt wordlah_old.txt')
+os.system('wget https://raw.githubusercontent.com/LanangAkira/Wp-Install-Finder/main/wordlah.txt --no-check-certificate')
 print("\033[32;1mUpdate Wordlist Success\n\033[0m")
